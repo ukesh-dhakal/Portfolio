22d4acf39e6998da2c03b378af44969a252072d5
Ukesh Dhakal <130669170+ukesh-dhakal@users.noreply.github.com>
2025-07-08 20:05:33 +0545

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
-// tailwind.config.js
-const {heroui} = require("@heroui/theme");
 
-/** @type {import('tailwindcss').Config} */
+const { heroui } = require("@heroui/theme");
+
+ @type {import('tailwindcss').Config} *
 module.exports = {
   content: [
-    "./node_modules/@heroui/theme/dist/components/navbar.js",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/components/navbar.js", // Merged content paths
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+        display: ["var(--font-concert-one)"],
+      },
+    },
   },
-  darkMode: "class",
-  plugins: [heroui()],
+  darkMode: "class", 
+  plugins: [heroui()], 
 };
