a583529fab523f02c3b2bd0875ab5f2c716903ea
Ukesh Dhakal <130669170+ukesh-dhakal@users.noreply.github.com>
2025-08-09 12:29:19 +0545

    Update Spotify.tsx

--- app/Components/Spotify.tsx
+++ app/Components/Spotify.tsx
@@ -14,7 +14,7 @@ const SpotifyNowPlaying = () => {
   return (
 
   <img
-    src={`https://spotify-github-profile.kittinanx.com/api/view.svg?uid=317dtavgsxwwm5jvkviexkhityay&cover_image=true&theme=novatorem&show_offline=false&background_color=121212&interchange=true&bar_color=53b14f&bar_color_cover=false&_=${timestamp}`}
+    src={`https://spotify-github-profile.kittinanx.com/api/view.svg?uid=317dtavgsxwwm5jvkviexkhityay&cover_image=true&theme=novatorem&show_offline=true&background_color=121212&interchange=true&bar_color=53b14f&bar_color_cover=false&_=${timestamp}`}
     alt="Now Playing on Spotify"
 
   />
